1968a8bd621e8abf58e43e6161b97321a5a7bac1
Yung-Chieh Lo <yjlou.tw@gmail.com>
2020-02-08 22:14:19 -0800

    test.js: add

---
+++ test.js
@@ -0,0 +1,47 @@
+var FAILED = 0;
+
+function EXPECT_GE(a, b, msg) {
+  if (msg === undefined) { msg = "";}
+  if (a < b) {
+    console.error("EXPECT_GE: Expected: (" + a + ") >= (" + b + "), but not. " + msg);
+    console.error(new Error().stack);
+    FAILED++;
+  }
+}
+
+function EXPECT_LE(a, b, msg) {
+  if (msg === undefined) { msg = "";}
+  if (a > b) {
+    console.error("EXPECT_LE: Expected: (" + a + ") <= (" + actual + "), but not. " + msg);
+    console.error(new Error().stack);
+    FAILED++;
+  }
+}
+
+function EXPECT_EQ(expected, actual, msg) {
+  if (msg === undefined) { msg = "";}
+  if (expected != actual) {
+    console.error("EXPECT_EQ: Expected: (" + expected + "), but actual: (" + actual + "). " + msg);
+    console.error(new Error().stack);
+    FAILED++;
+  }
+}
+
+function test() {
+  FAILED = 0;
+
+  // alogos.js
+  testGetOverlappedDuration();
+  testGetDistanceFromLatLonInMeters();
+  testGetRisk();
+  testCheckContact();
+
+  // utils.js
+  testConvKmlDateToTimestamp();
+
+  if (FAILED) {
+    console.error("[FAIL]");
+  } else {
+    console.log("[PASS]");
+  }
+}
