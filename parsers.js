b3bdeaa9618fce54a3ae8a791f9b903ca7cb22a7
Stimim Chen <death1048576@gmail.com>
2020-02-09 21:18:31 +0800

    parsers.js: refactor parseKml
    Extract the logic of parsing "Point" type into a function.
    Also extract the logic of parsing "name" and "timespan" tags, so we can
    reuse them in the future.
    
    TEST=the return value of parseKml() is the same

--- parsers.js
+++ parsers.js
@@ -1,3 +1,61 @@
+function tryParseName(placemark) {
+  const name_elem = placemark.getElementsByTagName("name");
+  if (name_elem.length == 1) {
+    return name_elem[0].innerHTML.trim();
+  }
+
+  console.error("Too many names: ", name_elem);
+  return "";
+}
+
+function tryParseTimeSpan(placemark) {
+  const timespan = placemark.getElementsByTagName("TimeSpan");
+  if (timespan.length == 1) {
+    const time_begin = timespan[0].children[0].innerHTML;
+    const time_end = timespan[0].children[1].innerHTML;
+    return {
+      begin: parseFloat(convKmlDateToTimestamp(time_begin.trim())),
+      end: parseFloat(convKmlDateToTimestamp(time_end.trim())),
+    }
+  } else {
+    // TODO: How about throwing an exception instead?
+    console.error("Invalid timespan data: ", placemark);
+    return null;
+  }
+}
+
+function tryParsePoint(placemark) {
+  var latlng = "";
+
+  const point = placemark.getElementsByTagName("Point");
+
+  if (point.length == 0) {
+    // Not point data. Maybe LineString data. Discard.
+    return null;
+  }
+  if (point.length != 1) {
+    console.error("Invalid point data: ", placemark);
+    return null;
+  }
+
+  latlng = point[0].children[0].innerHTML;
+
+  const name = tryParseName(placemark);
+  const timespan = tryParseTimeSpan(placemark);
+  if (timespan === null) {
+    return null;
+  }
+
+  // Point data: (-122.02223289999999,37.338164,0) 2020-02-07T16:14:56.673Z~2020-02-07T16:20:34.000Z
+  return [{
+    'lat': parseFloat(latlng.split(",")[1].trim()),
+    'lng': parseFloat(latlng.split(",")[0].trim()),
+    'begin': timespan.begin,
+    'end': timespan.end,
+    'name': name,
+  }];
+}
+
 // Given a KML text, returns an array of Point data.
 //
 function parseKml(text) {
@@ -10,48 +68,16 @@ function parseKml(text) {
   console.log(placemarks);
   for(var i = 0; i < placemarks.length; i++) {
     var placemark = placemarks[i];
-    var latlng = "";
-    var time_begin = "";
-    var time_end = "";
-    var name = "";
 
     // TODO: support 'address' type
     // TODO: support 'linestring' type
-    var point = placemark.getElementsByTagName("Point");
-    if (point.length == 0) {
-      // Not point data. Maybe LineString data. Discard.
-      continue;
-    } else if (point.length == 1) {
-      latlng = point[0].children[0].innerHTML;
-    } else {
-      console.error("Invalid point data: ", placemark);
+    let retval = tryParsePoint(placemark);
+    if (retval !== null) {
+      console.info(retval);
+      output.push(...retval);
       continue;
     }
 
-    var name_elem = placemark.getElementsByTagName("name");
-    if (name_elem.length == 1) {
-      name = name_elem[0].innerHTML;
-    } else {
-      console.error("Too many names: ", name_elem);
-    }
-
-    var timespan = placemark.getElementsByTagName("TimeSpan");
-    if (timespan.length == 1) {
-      time_begin = timespan[0].children[0].innerHTML;
-      time_end = timespan[0].children[1].innerHTML;
-    } else {
-      console.error("Invalid timespan data: ", placemark);
-      continue;
-    }
-
-    // Point data: (-122.02223289999999,37.338164,0) 2020-02-07T16:14:56.673Z~2020-02-07T16:20:34.000Z
-    output.push({
-      'lat': parseFloat(latlng.split(",")[1].trim()),
-      'lng': parseFloat(latlng.split(",")[0].trim()),
-      'begin': parseFloat(convKmlDateToTimestamp(time_begin.trim())),
-      'end': parseFloat(convKmlDateToTimestamp(time_end.trim())),
-      'name': name.trim(),
-    });
   }
 
   return output;
