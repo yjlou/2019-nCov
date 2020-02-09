96982a97edfb94179f344843dad646e848d6d7ab
Yung-Chieh Lo <yjlou.tw@gmail.com>
2020-02-08 22:14:06 -0800

    parsers.js: parse string data to float

--- parsers.js
+++ parsers.js
@@ -1,3 +1,5 @@
+// Given a KML text, returns an array of Point data.
+//
 function parseKml(text) {
   var parser = new DOMParser();
   var kml = parser.parseFromString(text,"text/xml");
@@ -13,6 +15,8 @@ function parseKml(text) {
     var time_end = "";
     var name = "";
 
+    // TODO: support 'address' type
+    // TODO: support 'linestring' type
     var point = placemark.getElementsByTagName("Point");
     if (point.length == 0) {
       // Not point data. Maybe LineString data. Discard.
@@ -27,6 +31,8 @@ function parseKml(text) {
     var name_elem = placemark.getElementsByTagName("name");
     if (name_elem.length == 1) {
       name = name_elem[0].innerHTML;
+    } else {
+      console.error("Too many names: ", name_elem);
     }
 
     var timespan = placemark.getElementsByTagName("TimeSpan");
@@ -34,17 +40,17 @@ function parseKml(text) {
       time_begin = timespan[0].children[0].innerHTML;
       time_end = timespan[0].children[1].innerHTML;
     } else {
-      console.error("Invalid timespane data: ", placemark);
+      console.error("Invalid timespan data: ", placemark);
       continue;
     }
 
     // Point data: (-122.02223289999999,37.338164,0) 2020-02-07T16:14:56.673Z~2020-02-07T16:20:34.000Z
     output.push({
-      'lat': latlng.split(",")[0],
-      'lng': latlng.split(",")[1],
-      'begin': time_begin,
-      'end': time_end,
-      'name': name,
+      'lat': parseFloat(latlng.split(",")[1].trim()),
+      'lng': parseFloat(latlng.split(",")[0].trim()),
+      'begin': parseFloat(time_begin.trim()),
+      'end': parseFloat(time_end.trim()),
+      'name': name.trim(),
     });
   }
 
