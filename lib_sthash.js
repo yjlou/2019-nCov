cb7504ef000aa7515b9303c9b1f487b38138c748
Yung-Chieh Lo <yjlou.tw@gmail.com>
2020-02-12 22:36:36 -0800

    lib_sthash.js:cfix test cases

--- lib_sthash.js
+++ lib_sthash.js
@@ -40,6 +40,9 @@
 // Then, all combination of above timestamps and lat/lng will be hashed
 // to its value for future comparison.
 //
+// TODO: hashSpacetime() supports dynamic N-level spread-out.
+// TODO: sthash.js generates 0-level spread-out; web matching uses N-level spread-out.
+//
 "use strict";
 
 var DEFAULT_HASH_KEY = "JuliaWu";
@@ -102,7 +105,7 @@ function HMAC(key) {
 
 function testHmac() {
   var hmac = HMAC("abc");
-  EXPECT_EQ("bfa8c5bc764321ac99301c8a616e9d96", hmac(123, 25.123, 122.123));
+  EXPECT_EQ("bfa8c5bc764321ac", hmac(123, 25.123, 122.123));
 }
 
 function hashSpacetime(key, begin, end, lat, lng){
@@ -129,16 +132,16 @@ function hashSpacetime(key, begin, end, lat, lng){
 function testHashSpacetime() {
   EXPECT_EQ([
     // timestamp 0
-    "c382a0e3cb70d9944d0d8ce0b75207fd", "c70af5fb7b8fde48e4a6c663c0ac65f7", "9eb8bf971bd6a5c43ef9d72521941b23",
-    "3edfc9c2b1918d943a4c66f5ec14801c", "6d01141a9aa0ccfa86a9c76a689c750a", "b677765bc46e842cc5c56c0b3738bc0d",
-    "4bc38a6e7d6ff3b17c2efd9e060c108a", "bb10b2665ae6ddfb4f7ce92439ba4d26", "3221754cc20782e28bc669f87639ef53",
+    "c382a0e3cb70d994", "c70af5fb7b8fde48", "9eb8bf971bd6a5c4",
+    "3edfc9c2b1918d94", "6d01141a9aa0ccfa", "b677765bc46e842c",
+    "4bc38a6e7d6ff3b1", "bb10b2665ae6ddfb", "3221754cc20782e2",
     // timestamp 600
-    "514f818e18001c5665b0cdf52d4387d0", "6d196070385fb8812ef472263aad9bbb", "3354367bcca7a39af17b8395c14b5312",
-    "38df5a16e00b8fdd22aacfbc22ba31d4", "2d2423974df2968c684d63475d8acf06", "dc37d585fefe04e2babc46eb8fdf2e2a",
-    "1d4093fde802236863f7aae6a7607e4f", "e3c6e6e96e94d4be58cbfbc99fb5ac19", "0cefa29f531ab9a5bff2d2bc663e2efa",
+    "514f818e18001c56", "6d196070385fb881", "3354367bcca7a39a",
+    "38df5a16e00b8fdd", "2d2423974df2968c", "dc37d585fefe04e2",
+    "1d4093fde8022368", "e3c6e6e96e94d4be", "0cefa29f531ab9a5",
     // timestamp 1200
-    "85ec3daafd12a58088ead221ab96a1f5", "546835556647f2876724b1cad4bdc4af", "46b4b9f876166efea00c4ad837195188",
-    "8f72ee403295fa96d1ecb641cbcfb6a3", "29dd5053e89e18e7bff63757c5d509ac", "e086767349312bc838a813f9fa4a98ce",
-    "f003d1589b89a76c0f9766d91a265580", "435ce8b5b333c9b09d65b8aef8eb73de", "0a79fc6b39705be6817cce71182cccca",
+    "85ec3daafd12a580", "546835556647f287", "46b4b9f876166efe",
+    "8f72ee403295fa96", "29dd5053e89e18e7", "e086767349312bc8",
+    "f003d1589b89a76c", "435ce8b5b333c9b0", "0a79fc6b39705be6",
   ], hashSpacetime("abc", 100, 700, 25.123456, 122.123000));
 }
