3b3cf74b303a2c4360f69467b45ac5285082b698
Stimim Chen <106878+Stimim@users.noreply.github.com>
2020-03-07 12:04:33 +0800

    countries/korea: add updater script for coronavirus-dataset

---
+++ countries/korea/coronavirus-dataset-updater.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+#
+# Download data from coronamap.site and convert it to Takeout JSON format.
+#
+# Due to the nodejs require path restriction, this script must be run under
+# countries/korea.
+#
+
+PREFIX="coronavirus-dataset"
+INPUT="${PREFIX}-input.js"
+OUTPUT="${PREFIX}-output.json"
+URL="https://raw.githubusercontent.com/jihoo-kim/Coronavirus-Dataset/master/route.csv"
+
+# Download the data file and manipulate a little bit.
+wget "${URL}" -O "${INPUT}"
+
+nodejs "${PREFIX}"-converter.js \
+    -i "${INPUT}" \
+    -o "${OUTPUT}"
