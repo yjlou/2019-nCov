e7c689ef7f14c9d57b0b0348de9de3c752ffa97f
talbenbasat <talbenbasat@gmail.com>
2020-03-14 15:49:13 +0200

    Updated data New package.json

--- countries/israel/update.sh
+++ countries/israel/update.sh
@@ -6,4 +6,4 @@ META=meta.json
 OUTPUT=output.json
 
 wget 'https://services5.arcgis.com/dlrDjz89gx9qyfev/arcgis/rest/services/Corona_Exposure_View/FeatureServer/0/query?f=json&where=1%3D1&returnGeometry=true&spatialRel=esriSpatialRelIntersects&outFields=*&maxRecordCountFactor=4&outSR=4326&resultOffset=0&resultRecordCount=8000&cacheHint=true' -O "${INPUT}"
-nodejs converter.js -i "${INPUT}" -o "${OUTPUT}" -m "${META}"
+node converter.js -i "${INPUT}" -o "${OUTPUT}" -m "${META}"
